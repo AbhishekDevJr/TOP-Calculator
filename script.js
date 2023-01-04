3c27bb1bae99d0b3aa0711ada8dfb5967d4e872f
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-01-04 13:38:13 +0530

    Operate Function to execute math operations

--- script.js
+++ script.js
@@ -22,4 +22,25 @@ function divide(value1, value2){
 }
 
 //Checking Functions
-console.log('50+100--> ' + add(50, 100) + ' 50-100--> ' + subtract(50, 100) + ' 50*100--> ' + multiply(50, 100) + ' 50/100--> ' + divide(50, 100));
+// console.log('50+100--> ' + add(50, 100) + ' 50-100--> ' + subtract(50, 100) + ' 50*100--> ' + multiply(50, 100) + ' 50/100--> ' + divide(50, 100));
+
+//Function that takes operator and two values and perform that operation on the values
+function operate(operator, value1, value2){
+    if(operator === '+'){
+        return (add(value1, value2));
+    }
+    else if (operator === '-'){
+        return (subtract(value1, value2));
+    }
+    else if (operator === '*'){
+        return (multiply(value1, value2));
+    }
+    else if (operator === '/'){
+        return (divide(value1, value2));
+    }
+    else{
+        alert('Please check the values or operator.');
+    }
+}
+
+console.log(operate('+', 10, 15));

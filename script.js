1d0dda73ca8d2bff3e40b4891087c8b89273a62e
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-01-04 13:21:02 +0530

    Initial File Setup + Basic Math Functions Developed

---
+++ script.js
@@ -0,0 +1,25 @@
+//JS File to build the functionalities for the TOP-Calculator Project.
+//Start by Creating functions to perform basic Calculator operations
+
+//Function to perform Addition Operation
+function add(value1, value2){
+    return (value1 + value2);
+}
+
+//Function to perform Subtraction Operation
+function subtract(value1, value2){
+    return (value1 - value2);
+}
+
+//Function to perform Multiplication Operation
+function multiply(value1, value2){
+    return (value1 * value2);
+}
+
+//Function to perform Division Operation
+function divide(value1, value2){
+    return (value1 / value2);
+}
+
+//Checking Functions
+console.log('50+100--> ' + add(50, 100) + ' 50-100--> ' + subtract(50, 100) + ' 50*100--> ' + multiply(50, 100) + ' 50/100--> ' + divide(50, 100));
